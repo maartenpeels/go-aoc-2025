2d7661abe25ad6f24efc15a2b78a45062511c002
Maarten Peels <maartenpeels@gmail.com>
2025-12-02 13:44:22 +0100

    Day 2 - Part 2

--- days/day02/solution.go
+++ days/day02/solution.go
@@ -9,11 +9,19 @@ import (
 )
 
 type Solution struct {
-	invalidIDs []int
 }
 
 func (s *Solution) Part1(input string) (string, error) {
+	return s.solve(input, true)
+}
+
+func (s *Solution) Part2(input string) (string, error) {
+	return s.solve(input, false)
+}
+
+func (s *Solution) solve(input string, exactTwo bool) (string, error) {
 	ranges := utils.SingleLineSplit(input, ",")
+	invalidIDs := []int{}
 
 	for _, r := range ranges {
 		parts := strings.Split(r, "-")
@@ -36,35 +44,53 @@ func (s *Solution) Part1(input string) (string, error) {
 		}
 
 		for i := lowerInt; i <= higherInt; i++ {
-			if s.isInvalid(i) {
-				s.invalidIDs = append(s.invalidIDs, i)
+			if s.isInvalid(i, exactTwo) {
+				invalidIDs = append(invalidIDs, i)
 			}
 		}
 	}
 
 	total := 0
-	for _, id := range s.invalidIDs {
+	for _, id := range invalidIDs {
 		total += id
 	}
 
 	return strconv.Itoa(total), nil
 }
 
-func (s *Solution) Part2(input string) (string, error) {
-	//ranges := utils.SingleLineSplit(input, ",")
+func (s *Solution) isInvalid(id int, exactTwo bool) bool {
+	idStr := strconv.Itoa(id)
+	idLen := len(idStr)
 
-	return "", nil
-}
+	for prefixLen := 1; prefixLen <= idLen/2; prefixLen++ {
+		if idLen%prefixLen != 0 {
+			continue
+		}
 
-func (s *Solution) isInvalid(id int) bool {
-	// Needs to be even length (chars)
-	if len(strconv.Itoa(id))%2 != 0 {
-		return false
+		prefix := idStr[:prefixLen]
+		repetitions := idLen / prefixLen
+
+		if exactTwo && repetitions != 2 {
+			continue
+		}
+		if !exactTwo && repetitions < 2 {
+			continue
+		}
+
+		isValid := true
+		for i := 0; i < repetitions; i++ {
+			start := i * prefixLen
+			end := start + prefixLen
+			if idStr[start:end] != prefix {
+				isValid = false
+				break
+			}
+		}
+
+		if isValid {
+			return true
+		}
 	}
 
-	// First half of digits need to be the same as the second half
-	firstHalf := strconv.Itoa(id)[:len(strconv.Itoa(id))/2]
-	secondHalf := strconv.Itoa(id)[len(strconv.Itoa(id))/2:]
-
-	return firstHalf == secondHalf
+	return false
 }
